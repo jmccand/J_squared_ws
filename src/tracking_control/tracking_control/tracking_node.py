75e40d6ce62b98a50f5f320a39a50ab1c49a7153
Joel McCandless <mail@joelmccandless.com>
2026-03-29 16:21:02 -0400

    make theta float

--- src/tracking_control/tracking_control/tracking_node.py
+++ src/tracking_control/tracking_control/tracking_node.py
@@ -245,7 +245,7 @@ class TrackingNode(Node):
         cmd_vel = Twist()
         cmd_vel.linear.x = -U_att[0]#  - U_rep[0]
         cmd_vel.linear.y = -U_att[1]#  - U_rep[1]
-        cmd_vel.angular.z = 0 # theta
+        cmd_vel.angular.z = 0.0 # theta
         return cmd_vel
     
         ############################################
